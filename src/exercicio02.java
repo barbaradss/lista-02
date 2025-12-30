a73537630e2fc6688c29ca10e3d882513396b03c
Barbara Silva <barbara.silva@zup.com.br>
2025-12-30 12:43:10 -0300

    Exercicios 1 e 2 finalizados. Prática de loops for e while

---
+++ src/exercicio02.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class exercicio02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Insira um número maior que 0");
+        int numero = sc.nextInt();
+
+        int resultado = 1 ;
+
+        if (numero > 0){
+            while(numero>=1){
+                resultado = resultado * numero;
+                numero--;
+            }
+            System.out.println(resultado);
+        }else {
+            System.out.println("Número inválido");
+        }
+        
+    }
+}

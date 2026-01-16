607be26b49b1debaa28bd42b58676e9ed06c6801
breno.otsuka <breno.otsuka@zup.com.br>
2026-01-16 11:00:53 -0300

    refact: exerc calcular determinante

--- src/DeterminanteMatriz.java
+++ src/DeterminanteMatriz.java
@@ -6,10 +6,7 @@ public class DeterminanteMatriz {
                 {7,8,9}
         };
 
-        int determinante =
-                matriz[0][0] * (matriz[1][1] * matriz[2][2] - matriz[1][2] * matriz[2][1])
-                - matriz[0][1] * (matriz[1][0] * matriz[2][2] - matriz[1][2] * matriz[2][0])
-                + matriz[0][2] * (matriz[1][0] * matriz[2][1] - matriz[1][1] * matriz[2][0]);
+        int determinante = calcularDeterminante(matriz);
 
         System.out.println("Matriz:");
         for (int i = 0; i < 3; i++) {
@@ -20,7 +17,44 @@ public class DeterminanteMatriz {
         }
 
         System.out.println("\nDeterminante = " + determinante);
+    }
 
+    public static int calcularDeterminante(int[][] matriz) {
+        int[][] matrizEstendida = estenderMatriz(matriz);
+        int somaPrincipal = 0;
+        for (int desloc = 0; desloc < 3; desloc++) {
+            somaPrincipal += produtoDiagonal(matrizEstendida, desloc);
+        }
+        int somaSecundaria = 0;
+        for (int desloc = 0; desloc < 3; desloc++) {
+            somaSecundaria += produtoDiagonalOposta(matrizEstendida, desloc);
+        }
+        return somaPrincipal - somaSecundaria;
+    }
 
+    private static int[][] estenderMatriz(int[][] matriz) {
+        int[][] matrizEstendida = new int[3][5];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 5; j++) {
+                matrizEstendida[i][j] = matriz[i][j % 3];
+            }
+        }
+        return matrizEstendida;
+    }
+
+    private static int produtoDiagonal(int[][] matriz, int deslocamento) {
+        int produto = 1;
+        for (int i = 0; i < 3; i++) {
+            produto *= matriz[i][i+deslocamento];
+        }
+        return produto;
+    }
+
+    private static int produtoDiagonalOposta(int[][] matriz, int deslocamento) {
+        int produto = 1;
+        for (int i = 0; i < 3; i++) {
+            produto *= matriz[2-i][i+deslocamento];
+        }
+        return produto;
     }
 }
